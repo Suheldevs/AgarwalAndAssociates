be027ef5ddab5eb0f26404c765a02000625433b6
Suheldevs <mohdsuhel.dev@gmail.com>
2025-04-16 18:05:04 +0530

    netlify issue

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import HeroSection from '../components/HeroSection'
 import AboutUsSection from '../components/AboutSection'
 import DirectorSection from '../components/DirectorSection'
 import ProjectSection from '../components/ProjectSection'
-import TestimonialSection from '../components/TEstimonialSection'
+import TestimonialSection from '../components/TestimonialSection'
 import CTASection from '../components/CtaSection'
 import BlogSection from '../components/BlogSection'
 
